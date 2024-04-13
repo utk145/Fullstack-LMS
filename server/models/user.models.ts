5299d3d9319f6a1a340c3761f21bfdf8969ecc30
utk145 <utkarsh20241a0514@grietcollege.com>
2024-04-13 16:17:27 +0530

    server: added user model design

---
+++ server/models/user.models.ts
@@ -0,0 +1,102 @@
+import mongoose, { Document } from "mongoose";
+import bcrypt from "bcryptjs";
+
+/**
+ * Regular expression pattern for validating email addresses.
+ */
+const emailRegexPattern: RegExp = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+/**
+ * Interface representing a user document in the database.
+ */
+export interface IUser extends Document {
+    name: string;
+    email: string;
+    password: string;
+    avatar: {
+        public_id: string;
+        url: string;
+    };
+    role: string;
+    isVerified: boolean;
+    courses: Array<{ courseId: string }>;
+    comparePassword: (password: string) => Promise<boolean>;
+}
+
+
+/**
+ * Mongoose schema for the user collection.
+ */
+const userSchema = new mongoose.Schema<IUser>({
+    name: {
+        type: String,
+        // required:true,
+        required: [true, "Name is a mandatory field"],
+    },
+    email: {
+        type: String,
+        required: [true, "Email is mandatory field"],
+        unique: true,
+        match: emailRegexPattern,
+        validate: {
+            validator: function (value: string) {
+                return emailRegexPattern.test(value);
+            },
+            message: "Please enter a valid email",
+        }
+    },
+    password: {
+        type: String,
+        required: [true, "Password is mandatory field"],
+        minlength: [6, "Length of the password must be atleast 6 characters"]
+    },
+    avatar: {
+        public_id: {
+            type: String,
+            required: true,
+        },
+        url: {
+            type: String,
+            required: true,
+        },
+    },
+    role: {
+        type: String,
+        enum: ["user", "admin"],
+        default: "user",
+    },
+    isVerified: {
+        type: Boolean,
+        default: false,
+    },
+    courses: [
+        {
+            courseId: String
+        },
+    ],
+}, { timestamps: true });
+
+
+// encrypting the password. we use pre hook(a middleware) so that we encrpyt it just before saving 
+/**
+ * Middleware to encrypt the password before saving the user document.
+ */
+userSchema.pre<IUser>("save", async function (next) {
+    if (this.isModified("password")) {
+        this.password = await bcrypt.hash(this.password, 10);
+        next();
+    }
+    next();
+});
+
+// but now we need a method to compare the original password with the encrypted one from db
+/**
+ * Method to compare the provided password with the stored hashed password.
+ * @param password The password to compare.
+ * @returns A Promise resolving to true if the passwords match, false otherwise.
+ */
+userSchema.methods.comparePassword = async function (password: string): Promise<boolean> {
+    return await bcrypt.compare(password, this.password);
+};
+
+export const User = mongoose.model<IUser>("User", userSchema);
--- server/notes.md
+++ server/notes.md
@@ -3,3 +3,15 @@ BASIC SETUP:
     - npm init
 
     - npm i bcryptjs cookie-parser cors express ioredis jsonwebtoken mongoose ts-node-dev @types/bcryptjs @types/cookie-parser @types/cors @types/express @types/node @types/jsonwebtoken typescript
+
+
+## PLAN:     
+### Handle Errors and User authentication
+1. Handle errors ✅
+2. User model design ✅
+3. User registration
+4. User activation
+5. User login, logout
+6. User authorization
+7. Generate token, user socials auth, get user info
+8. Update user info, password and avatar 
